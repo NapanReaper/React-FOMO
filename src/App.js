9ffcf22c2a2820186cc4b1659ca0f57929d725e8
Do Tran Nhat Duy <napanreaper2506@gmail.com>
2021-02-26 11:26:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import './App.css';
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+import { useEffect, useRef, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+import SendInvites from './components/SendInvites';
+import SignUp from './components/SignUp';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIlke1MpAxgAGZPa2O9-NDYN_H19MuSLE",
+  authDomain: "react-fomo.firebaseapp.com",
+  projectId: "react-fomo",
+  storageBucket: "react-fomo.appspot.com",
+  messagingSenderId: "547631659710",
+  appId: "1:547631659710:web:555643865014124908bcef",
+  measurementId: "G-6XR8KXHHXF"
+};
+
+if (!firebase.app.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth)
+
   return (
     <div className="App">
+      <header className="App-header">
+        {user ? <SendInvites user={user} /> : <SignUp />}
+      </header>
     </div>
   );
 }
---
+++ src/components/SendInvites.jsx
@@ -0,0 +1,5 @@
+const SendInvites = () => {
+  return <div></div>;
+};
+
+export default SendInvites;
